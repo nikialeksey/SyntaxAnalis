38087e4bdc129b43c165e176b95b8a625bc778b6
Aleksey <nikialeksey@gmail.com>
2014-11-17 11:06:20 +0600

    started lab 10

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from Analisys import Scanner
 from Analisys import Syntax
+from Analisys import LL1
 
 fprog = open("input.txt", "r")
 program = ""
 for line in fprog:
     program += line
 program += "\0"
+fprog.close()
 
 scanner = Scanner(program)
-syntax = Syntax(scanner)
+# syntax = Syntax(scanner)
+syntax = LL1(scanner)
 
 try:
-    syntax.main_program()
+    syntax.run()
 except Exception as e:
     print(e)
 else:
